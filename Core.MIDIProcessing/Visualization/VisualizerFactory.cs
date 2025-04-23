8c82a2ea69470034453abe415eb302edda74df76
Nataliia Avierina <avierina.nataliia@gmail.com>
2025-04-23 23:23:28 +0300

    refactor: remove unnecessary code

--- Core.MIDIProcessing/Visualization/VisualizerFactory.cs
+++ Core.MIDIProcessing/Visualization/VisualizerFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.MIDIProcessing.Helpers;
+﻿using Core.MIDIProcessing.Helpers;
 
 namespace Core.MIDIProcessing.Visualization
 {
